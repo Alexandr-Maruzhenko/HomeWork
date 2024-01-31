7049c0889d0e13d05b33c9439f1488cf1c54dc9a
Alexandr <alexandr780113@gmail.com>
2024-01-31 13:38:47 +0300

    The verification method has been changed to counting minuses

--- Lesson_3/task_4.py
+++ Lesson_3/task_4.py
@@ -1,37 +1,14 @@
 # Пользователь вводит 3 числа, сказать сколько из них положительных и сколько отрицательных
 #
-# ОБНУЛЕНИЕ СЧЁТЧИКОВ ----------------------------------
-negative_number_counter = 0
-positive_number_counter = 0
-zero_counter = 0
-#
 # ЗАПРОС ЧИСЕЛ -----------------------------------------
-number_1 = int(input("Введите число №1: "))
-number_2 = int(input("Введите число №2: "))
-number_3 = int(input("Введите число №3: "))
+number_1 = input("Введите число №1: ")
+number_2 = input("Введите число №2: ")
+number_3 = input("Введите число №3: ")
 #
-# ПРОВЕРКИ (ПРИМИТИВНО И НЕ КРАСИВО )-------------------
-if number_1 < 0:
-    negative_number_counter += 1
-elif number_1 == 0:
-    zero_counter += 1
-else:
-    positive_number_counter += 1
-
-if number_2 < 0:
-    negative_number_counter += 1
-elif number_2 == 0:
-    zero_counter += 1
-else:
-    positive_number_counter += 1
-
-if number_3 < 0:
-    negative_number_counter += 1
-elif number_3 == 0:
-    zero_counter += 1
-else:
-    positive_number_counter += 1
+# ПОДСЧЁТ СИМВОЛОВ "МИНУС" В ЧИСЛАХ --------------------
+negative_counter = (number_1+number_2+number_3).count("-")
 #
 # ФОРМИРОВАНИЕ И ВЫВОД РЕЗУЛЬТАТА ----------------------
-message = f"Отрицательных чисел {negative_number_counter}, положительных чисел {positive_number_counter}, нулей {zero_counter}"
-print(message)
+message = f"Отрицательных чисел {negative_counter}, положительных чисел {3-negative_counter}"
+print(message, end=".")
+
