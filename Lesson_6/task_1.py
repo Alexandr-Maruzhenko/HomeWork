e2242912d24fb851f407f7905843753c23e3ea47
Alexandr <alexandr780113@gmail.com>
2024-02-12 01:56:53 +0300

    Initial commit

--- Lesson_6/task_1.py
+++ Lesson_6/task_1.py
@@ -1,6 +1,6 @@
 # Написать функцию перевода десятичного числа в двоичное и обратно, без использования функции int
 
-def converter(x):
+def converter_bin(x):
     binary = []
     string_binary = ""
     while x // 2:
@@ -13,5 +13,16 @@ def converter(x):
     return string_binary
 
 
-a = converter(int(num := input("Введите целое число: ")))
-print(f"Число {num} в десятичной системе = числу {a} в двоичной системе.")
+def converter_dec(x):
+    number_dec = 0
+    binary_list = list(map(int, list(x)))
+    binary_list.reverse()
+    for id, val in enumerate(binary_list):
+        number_dec += val * pow(2, id)
+    return number_dec
+
+
+numb_bin = converter_bin(int(num := input("Введите целое число: ")))
+print(f"Число {num} в десятичной системе = числу {numb_bin} в двоичной системе.")
+numb_dec = converter_dec(numb_bin)
+print(f"Число {numb_bin} в двоичной системе = числу {numb_dec} в десятичной системе.")
